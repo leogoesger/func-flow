447269cbc1ee358c6d7727d421f3edbd500bb815
Noelle <noellekpatterson@gmail.com>
2018-01-30 11:50:53 -0800

    update start of summer to operate on Oct water year, and add capability to calculate by class or gage. Still working on minor fixes for optimized results.

--- utils/calc_start_of_summer.py
+++ utils/calc_start_of_summer.py
@@ -7,18 +7,32 @@ def calc_start_of_summer(matrix, start_date):
     start_dates = []
 
     for index, flow in enumerate(matrix[0]):
-        twenty_percentile = np.nanpercentile(matrix[:, index], 20)
+        # calculate the percentile which gives the 73 lowest flow days in the second half of the water year
+        percentile = np.nanpercentile(matrix[182:len(matrix[:]-1)], 27)
+        
         smooth_data = moving_average(matrix[:, index])
+        search_range = smooth_data.index(max(smooth_data))
 
         for data_index, data in enumerate(smooth_data):
 
-            if (data_index >= len(smooth_data) - 6):
+            if (data_index >= len(smooth_data) - 4):
                 start_dates.append(float('NaN'))
                 break
-            elif data_index > 75 and data <= twenty_percentile and smooth_data[data_index + 1] <= twenty_percentile and \
-                smooth_data[data_index + 2] <= twenty_percentile and smooth_data[data_index + 3] <= twenty_percentile and smooth_data[data_index + 4] <= twenty_percentile:
+            elif data_index >= search_range and data <= percentile and smooth_data[data_index + 1] <= percentile and \
+                smooth_data[data_index + 2] <= percentile and smooth_data[data_index + 3] <= percentile:
                 start_dates.append(data_index)
                 break
+            
+    failed_calcs = 0
+    for index, dates in enumerate(start_dates):
+        if start_dates[index] == search_range:
+            start_dates[index] == np.nan
+            failed_calcs = failed_calcs + 1
+        elif start_dates[index] == 0:
+            start_dates[index] == np.nan
+            failed_calcs = failed_calcs + 1
+
+    print('Test failed for {} out of {} water years.'.format(failed_calcs, matrix.shape[1]))
 
         # if get_nan_fraction_in_array(matrix[:, index]) > 0.2:
         #     continue
@@ -30,7 +44,7 @@ def calc_start_of_summer(matrix, start_date):
         #     plt.xlabel('Julian Day')
         #     plt.ylabel('Flow, ft^3/s')
         #     plt.axvline(start_dates[index], color='red')
-        #     plt.axhline(twenty_percentile)
+        #     plt.axhline(percentile)
         #     plt.savefig('processedFiles/StartSummer/{}.png'.format(index+1))
-
+    print(start_dates)
     return start_dates
