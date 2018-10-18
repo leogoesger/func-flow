909506d0e6d4b0ef73ee3c3b59b7843788462bad
Noelle Patterson <noellekpatterson@gmail.com>
2018-10-17 18:13:09 -0700

    test

--- calculations/FallWinterBaseflow.py
+++ calculations/FallWinterBaseflow.py
@@ -15,6 +15,8 @@ class FallWinterBaseflow(Abstract):
         self.gauge_number_array = []
         self.wet_baseflows = {}
 
+        test test test 
+
         for percent in self.percentilles:
             self.wet_baseflows[percent] = []
 
