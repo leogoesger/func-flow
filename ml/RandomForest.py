c605468cc0823cb0820e77b17d5e23cc16071afd
Leo Q <leoq91@gmail.com>
2019-02-02 23:48:43 -0800

    retrain

--- ml/RandomForest.py
+++ ml/RandomForest.py
@@ -1,14 +1,15 @@
 import operator
-# import pydot
+import pydot
 import numpy as np
 import pandas as pd
-from fancyimpute import KNN
+# from fancyimpute import KNN
 from joblib import dump, load
 from sklearn import preprocessing
-# from sklearn.tree import export_graphviz
+from sklearn.tree import export_graphviz
+from sklearn.impute import SimpleImputer
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 class RandomForest:
@@ -17,6 +18,7 @@ class RandomForest:
     n_estimators = 500
     file_path = "./ml"
     dump_path = "./ml/rf.joblib"
+    imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 
     def __init__(self):
 
@@ -43,7 +45,9 @@ class RandomForest:
         self.features_np = self.scaler.transform(self.features_np)
 
     def impute(self):
-        self.features_np = KNN(k=5).fit_transform(self.features_np)
+        # self.features_np = KNN(k=5).fit_transform(self.features_np)
+        self.imputer = self.imputer.fit(self.features_np)
+        self.features_np = self.imputer.transform(self.features_np)
 
     def split(self):
         self.train_features, self.test_features, self.train_labels, self.test_labels = train_test_split(
@@ -79,7 +83,7 @@ class RandomForest:
         predictions = []
 
         _test = self.scaler.transform(np.array(metrics))
-        _test = KNN(k=5).fit_transform(_test)
+        _test = self.imputer.transform(_test)
         summary = self.rf.predict(_test)
 
         for s in summary:
@@ -91,45 +95,45 @@ class RandomForest:
 
         return predictions
 
-    # def get_prediction_error(self):
-    #     valid = []
+    def get_prediction_error(self):
+        valid = []
 
-    #     for i, p in enumerate(self.predictions_s):
-    #         correct = p == self.test_labels[i]
-    #         valid.append(1) if np.all(correct) else valid.append(0)
+        for i, p in enumerate(self.predictions_s):
+            correct = p == self.test_labels[i]
+            valid.append(1) if np.all(correct) else valid.append(0)
 
-    #     print("Accuracy is:", sum(valid) / len(valid))
+        print("Accuracy is:", sum(valid) / len(valid))
 
-    # def get_tree(self, num):
-    #     tree = self.rf.estimators_[num]
-    #     dot_path = self.file_path + "/tree.dot"
-    #     png_path = self.file_path + "/tree.png"
+    def get_tree(self, num):
+        tree = self.rf.estimators_[num]
+        dot_path = self.file_path + "/tree.dot"
+        png_path = self.file_path + "/tree.png"
 
-    #     export_graphviz(tree, out_file=dot_path,
-    #                     feature_names=self.feature_list, rounded=True, precision=1)
-    #     (graph, ) = pydot.graph_from_dot_file(dot_path)
-    #     graph.write_png(png_path)
+        export_graphviz(tree, out_file=dot_path,
+                        feature_names=self.feature_list, rounded=True, precision=1)
+        (graph, ) = pydot.graph_from_dot_file(dot_path)
+        graph.write_png(png_path)
 
-    # def get_importance_factor(self):
-    #     self.importances = list(self.rf.feature_importances_)
-    #     feature_importances = [(feature, round(importance, 2))
-    #                            for feature, importance in zip(self.feature_list, self.importances)]
-    #     feature_importances = sorted(
-    #         feature_importances, key=lambda x: x[1], reverse=True)
-    #     [print('Variable: {:20} Importance: {}'.format(*pair))
-    #      for pair in feature_importances]
+    def get_importance_factor(self):
+        self.importances = list(self.rf.feature_importances_)
+        feature_importances = [(feature, round(importance, 2))
+                               for feature, importance in zip(self.feature_list, self.importances)]
+        feature_importances = sorted(
+            feature_importances, key=lambda x: x[1], reverse=True)
+        [print('Variable: {:20} Importance: {}'.format(*pair))
+         for pair in feature_importances]
 
-    # def plot_importance(self):
+    def plot_importance(self):
 
-    #     png_path = self.file_path + "/imp.png"
-    #     y_pos = np.arange(len(self.importances))
-    #     plt.barh(y_pos, self.importances)
-    #     plt.yticks(y_pos, self.feature_list)
+        png_path = self.file_path + "/imp.png"
+        y_pos = np.arange(len(self.importances))
+        plt.barh(y_pos, self.importances)
+        plt.yticks(y_pos, self.feature_list)
 
-    #     plt.ylabel('Variable ')
-    #     plt.xlabel('Importance')
-    #     plt.title('Variable Importances')
-    #     plt.savefig(png_path, bbox_inches="tight")
+        plt.ylabel('Variable ')
+        plt.xlabel('Importance')
+        plt.title('Variable Importances')
+        plt.savefig(png_path, bbox_inches="tight")
 
 
 # rf = RandomForest()
@@ -138,7 +142,7 @@ class RandomForest:
 # rf.split()
 # rf.train()
 # rf.dump()
-# rf.load()
+# # rf.load()
 # rf.predict()
 # rf.get_prediction_error()
 # rf.get_tree(5)
