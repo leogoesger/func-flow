3a08176e932fce6fa056574c40de38012d4976c3
Leo Q <leoq91@gmail.com>
2019-02-02 23:00:40 -0800

    update flask server for ml model

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,14 @@
 from datetime import datetime
-#import itertools
-#import numpy as np
-#import pandas as pd
 from flask import request, jsonify
 import simplejson
 from app import app
 from utils.matrix_convert import MatrixConversion
-#from calculations.AllMetrics import Metrics
-#from utils.helpers import remove_offset_from_julian_date
 from utils.upload_files import get_result
+from ml.RandomForest import RandomForest
+
+rf = RandomForest()
+rf.scale()
+rf.load()
 
 
 @app.route('/api', methods=['GET', 'POST'])
@@ -26,68 +26,10 @@ def index():
     return jsonify(simplejson.dumps(result, ignore_nan=True)), 200
 
 
-# def get_result(matrix, julian_start_date, params):
+@app.route("/api/class-predict", methods=["POST"])
+def predict():
+    req_body = request.get_json()
+    metric = req_body["metric"]
+    predictions = rf.predict_s(metric)
 
-#     result = {}
-#     result["year_ranges"] = [int(i) + 1 for i in matrix.year_array]
-#     result["flow_matrix"] = np.where(
-#         pd.isnull(matrix.flow_matrix), None, matrix.flow_matrix).tolist()
-#     result["start_date"] = matrix.start_date
-
-#     calculated_metrics = Metrics(
-#         matrix.flow_matrix, matrix.years_array, None, None, params)
-
-#     result["DRH"] = calculated_metrics.drh
-
-#     result["all_year"] = {}
-#     result["all_year"]["average_annual_flows"] = calculated_metrics.average_annual_flows
-#     result["all_year"]["standard_deviations"] = calculated_metrics.standard_deviations
-#     result["all_year"]["coefficient_variations"] = calculated_metrics.coefficient_variations
-
-#     result["winter"] = {}
-#     # Convert key from number to names
-#     key_maps = {2: "two", 5: "five", 10: "ten", 20: "twenty", 50: "fifty"}
-#     winter_timings = {}
-#     winter_durations = {}
-#     winter_magnitudes = {}
-#     winter_frequencys = {}
-#     for key, value in key_maps.items():
-#         winter_timings[value] = list(map(
-#             remove_offset_from_julian_date, calculated_metrics.winter_timings[key], itertools.repeat(julian_start_date)))
-#         winter_durations[value] = calculated_metrics.winter_durations[key]
-#         winter_magnitudes[value] = calculated_metrics.winter_magnitudes[key]
-#         winter_frequencys[value] = calculated_metrics.winter_frequencys[key]
-
-#     result["winter"]["timings"] = winter_timings
-#     result["winter"]["durations"] = winter_durations
-#     result["winter"]["magnitudes"] = winter_magnitudes
-#     result["winter"]["frequencys"] = winter_frequencys
-
-#     result["fall"] = {}
-#     result["fall"]["timings"] = list(map(
-#         remove_offset_from_julian_date, calculated_metrics.fall_timings, itertools.repeat(julian_start_date)))
-#     result["fall"]["magnitudes"] = calculated_metrics.fall_magnitudes
-#     result["fall"]["wet_timings"] = list(map(
-#         remove_offset_from_julian_date, calculated_metrics.fall_wet_timings, itertools.repeat(julian_start_date)))
-#     result["fall"]["durations"] = calculated_metrics.fall_durations
-
-#     result["summer"] = {}
-#     result["summer"]["timings"] = list(map(
-#         remove_offset_from_julian_date, calculated_metrics.summer_timings, itertools.repeat(julian_start_date)))
-#     result["summer"]["magnitudes_ten"] = calculated_metrics.summer_10_magnitudes
-#     result["summer"]["magnitudes_fifty"] = calculated_metrics.summer_50_magnitudes
-#     result["summer"]["durations_flush"] = calculated_metrics.summer_flush_durations
-#     result["summer"]["durations_wet"] = calculated_metrics.summer_wet_durations
-#     result["summer"]["no_flow_counts"] = calculated_metrics.summer_no_flow_counts
-
-#     result["spring"] = {}
-#     result["spring"]["timings"] = list(map(
-#         remove_offset_from_julian_date, calculated_metrics.spring_timings, itertools.repeat(julian_start_date)))
-#     result["spring"]["magnitudes"] = calculated_metrics.spring_magnitudes
-#     result["spring"]["durations"] = calculated_metrics.spring_durations
-#     result["spring"]["rocs"] = calculated_metrics.spring_rocs
-
-#     result["fall_winter"] = {}
-#     result["fall_winter"]["baseflows"] = calculated_metrics.wet_baseflows
-
-#     return result
+    return jsonify(predictions), 200
--- ml/RandomForest.py
+++ ml/RandomForest.py
@@ -1,15 +1,14 @@
-import pydot
 import operator
+# import pydot
 import numpy as np
 import pandas as pd
 from fancyimpute import KNN
 from joblib import dump, load
 from sklearn import preprocessing
-from sklearn.impute import SimpleImputer
-from sklearn.tree import export_graphviz
+# from sklearn.tree import export_graphviz
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 class RandomForest:
@@ -18,7 +17,6 @@ class RandomForest:
     n_estimators = 500
     file_path = "./ml"
     dump_path = "./ml/rf.joblib"
-    imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 
     def __init__(self):
 
@@ -33,18 +31,27 @@ class RandomForest:
         self.test_labels = None
 
         self.rf = None
+        self.scaler = None
         self.predictions = None  # this is given in percentage for each class
         self.predictions_s = []  # this is 0 or 1 for each class
+        self.orders = ['eight', 'five', 'four', 'nine',
+                       'one', 'seven', 'six', 'three', 'two']
         self.importances = None
 
+    def scale(self):
+        self.scaler = preprocessing.StandardScaler().fit(self.features_np)
+        self.features_np = self.scaler.transform(self.features_np)
+
     def impute(self):
-        self.features_np = preprocessing.scale(self.features_np)
         self.features_np = KNN(k=5).fit_transform(self.features_np)
 
     def split(self):
         self.train_features, self.test_features, self.train_labels, self.test_labels = train_test_split(
             self.features_np, self.labels, test_size=0.1, random_state=42)
-        self.train_labels = np.array(pd.get_dummies(self.train_labels))
+
+        self.train_labels = pd.get_dummies(self.train_labels)
+        self.orders = list(self.train_labels.columns)
+        self.train_labels = np.array(self.train_labels)
         self.test_labels = np.array(pd.get_dummies(self.test_labels))
 
     def train(self):
@@ -60,61 +67,80 @@ class RandomForest:
 
     def predict(self):
         self.predictions = self.rf.predict(self.test_features)
+
         for p in self.predictions:
             index, _ = max(enumerate(p), key=operator.itemgetter(1))
             temp_a = np.zeros(9)
             temp_a[index] = 1
             self.predictions_s.append(temp_a)
 
-    def get_prediction_error(self):
-        valid = []
+    def predict_s(self, metrics):
 
-        for i, p in enumerate(self.predictions_s):
-            correct = p == self.test_labels[i]
-            valid.append(1) if np.all(correct) else valid.append(0)
+        predictions = []
 
-        print("Accuracy is:", sum(valid) / len(valid))
+        _test = self.scaler.transform(np.array(metrics))
+        _test = KNN(k=5).fit_transform(_test)
+        summary = self.rf.predict(_test)
 
-    def get_tree(self, num):
-        tree = self.rf.estimators_[num]
-        dot_path = self.file_path + "/tree.dot"
-        png_path = self.file_path + "/tree.png"
+        for s in summary:
+            index, _ = max(enumerate(s), key=operator.itemgetter(1))
+            pred = {"class": self.orders[index], "summary": {}}
+            for i, key in enumerate(self.orders):
+                pred["summary"][key] = s[i]
+            predictions.append(pred)
 
-        export_graphviz(tree, out_file=dot_path,
-                        feature_names=self.feature_list, rounded=True, precision=1)
-        (graph, ) = pydot.graph_from_dot_file(dot_path)
-        graph.write_png(png_path)
+        return predictions
 
-    def get_importance_factor(self):
-        self.importances = list(self.rf.feature_importances_)
-        feature_importances = [(feature, round(importance, 2))
-                               for feature, importance in zip(self.feature_list, self.importances)]
-        feature_importances = sorted(
-            feature_importances, key=lambda x: x[1], reverse=True)
-        [print('Variable: {:20} Importance: {}'.format(*pair))
-         for pair in feature_importances]
+    # def get_prediction_error(self):
+    #     valid = []
 
-    def plot_importance(self):
+    #     for i, p in enumerate(self.predictions_s):
+    #         correct = p == self.test_labels[i]
+    #         valid.append(1) if np.all(correct) else valid.append(0)
 
-        png_path = self.file_path + "/imp.png"
-        y_pos = np.arange(len(self.importances))
-        plt.barh(y_pos, self.importances)
-        plt.yticks(y_pos, self.feature_list)
+    #     print("Accuracy is:", sum(valid) / len(valid))
 
-        plt.ylabel('Variable ')
-        plt.xlabel('Importance')
-        plt.title('Variable Importances')
-        plt.savefig(png_path, bbox_inches="tight")
+    # def get_tree(self, num):
+    #     tree = self.rf.estimators_[num]
+    #     dot_path = self.file_path + "/tree.dot"
+    #     png_path = self.file_path + "/tree.png"
+
+    #     export_graphviz(tree, out_file=dot_path,
+    #                     feature_names=self.feature_list, rounded=True, precision=1)
+    #     (graph, ) = pydot.graph_from_dot_file(dot_path)
+    #     graph.write_png(png_path)
+
+    # def get_importance_factor(self):
+    #     self.importances = list(self.rf.feature_importances_)
+    #     feature_importances = [(feature, round(importance, 2))
+    #                            for feature, importance in zip(self.feature_list, self.importances)]
+    #     feature_importances = sorted(
+    #         feature_importances, key=lambda x: x[1], reverse=True)
+    #     [print('Variable: {:20} Importance: {}'.format(*pair))
+    #      for pair in feature_importances]
+
+    # def plot_importance(self):
+
+    #     png_path = self.file_path + "/imp.png"
+    #     y_pos = np.arange(len(self.importances))
+    #     plt.barh(y_pos, self.importances)
+    #     plt.yticks(y_pos, self.feature_list)
+
+    #     plt.ylabel('Variable ')
+    #     plt.xlabel('Importance')
+    #     plt.title('Variable Importances')
+    #     plt.savefig(png_path, bbox_inches="tight")
 
 
-rf = RandomForest()
-rf.impute()
-rf.split()
+# rf = RandomForest()
+# rf.scale()
+# rf.impute()
+# rf.split()
 # rf.train()
 # rf.dump()
-rf.load()
-rf.predict()
-rf.get_prediction_error()
-rf.get_tree(5)
-rf.get_importance_factor()
-rf.plot_importance()
+# rf.load()
+# rf.predict()
+# rf.get_prediction_error()
+# rf.get_tree(5)
+# rf.get_importance_factor()
+# rf.plot_importance()
